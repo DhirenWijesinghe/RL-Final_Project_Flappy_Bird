d6c9257febc00c448bf59f93060a2911f36949cf
DhirenWijesinghe <d.wijesinghe@utexas.edu>
2022-05-08 21:24:52 -0500

    Cleaned up more

--- plot_data.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-
-df = pd.read_csv('scores_151.csv', names=['episode','score'])
-df = df.iloc[21: , :]
-
-# df.plot(x='episode', y='score')
-plt.plot(df["episode"], df["score"])
-plt.show()
